fa09b92975405284d8c561babd2fc726deae1bfa
adrian <alopezbanobre@gmail.com>
2024-11-05 18:40:58 -0300

    controlador productos

--- bd/conexion.js
+++ bd/conexion.js
@@ -6,11 +6,6 @@ const conexion = async() => {
 
         await mongoose.connect("proyectopannol-shard-00-00.dcs6l.mongodb.net:27017");
 
-        // Parametros dentro de objeto // solo en caso de aviso
-        // useNewUrlParser: true
-        // useUnifiedTopology: true
-        // useCreateIndex: true
-
         console.log("Conectado correctamente a la base de datos mi_blog !!");
 
     } catch(error) {
